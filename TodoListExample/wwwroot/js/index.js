cac1545e6bb7fb5d2e46adde3542088cf25dc33d
Kitty <nightmare.kitty111@gmail.com>
2021-04-11 22:17:22 +0800

    🎨 調整 js function 結構, 重複的程式碼抽出來獨立一個 function

--- TodoListExample/wwwroot/js/index.js
+++ TodoListExample/wwwroot/js/index.js
@@ -1,19 +1,19 @@
 (function () {
+  initGet();
+})();
+
+function initGet() {
   $.ajax({
     method: 'GET',
     url: '/api/Todo'
   }).done(function (data) {
     if (data.length > 0) {
       for (i = 0; i < data.length; i++) {
-        var cloneItem = $('#ItemTemplate').clone();
-        cloneItem.attr("id", data[i].todoID);
-        cloneItem.removeAttr('hidden');
-        cloneItem.find("label#ItemContent").text(data[i].todoContent);
-        $('#ItemList').append(cloneItem);
+        addTodoItem(data[i].todoID, data[i].todoContent);
       }
     }
   });
-})();
+}
 
 function add() {
   var todoContent = $('#todoText').val();
@@ -24,10 +24,14 @@ function add() {
     dataType: 'json',
     contentType: 'application/json'
   }).done(function (data) {
-    var cloneItem = $('#ItemTemplate').clone();
-    cloneItem.attr("id", data);
-    cloneItem.removeAttr('hidden');
-    cloneItem.find("label#ItemContent").text(todoContent);
-    $('#ItemList').append(cloneItem);
+    addTodoItem(data, todoContent);
   });
+}
+
+function addTodoItem(todoID, todoContent) {
+  var cloneItem = $('#ItemTemplate').clone();
+  cloneItem.attr("id", todoID);
+  cloneItem.removeAttr('hidden');
+  cloneItem.find("label#ItemContent").text(todoContent);
+  $('#ItemList').append(cloneItem);
 }
